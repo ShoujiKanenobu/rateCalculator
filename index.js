e1ecd4dc2180159971de11a17cb5fa8b055e6a59
ShoujiKanenobu <shoujikanenobu@gmail.com>
2019-11-16 16:17:38 -0800

    added calculation and view

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .get('/times', (req, res) => res.send(showTimes()))
-  .get('/getRate', (req, res) => res.render('pages/getRate'))
+  .get('/getRate', (req, res) => {
+    let rate = calculateRate(req)
+     res.render('pages/getRate', rate)
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
   showTimes = () => {
@@ -20,4 +23,65 @@ express()
     result += i + ' '
   }
   return result;
+
+  function calculateRate(req) {
+    let mailType = req.query.mailType;
+    let weight = parseInt(req.query.weight);
+    let rate = 0;
+
+    switch(mailType)
+    {
+      case 'LettersStamped':
+          if(weight < 1)
+          {
+              rate = 0.55;
+              weight--;
+          }
+          while(weight < 0)
+          {
+            rate += 0.15;
+            weight--;
+          }
+
+      break;
+      case 'LettersMetered':
+          if(weight < 1)
+          {
+              rate = 0.5;
+              weight--;
+          }
+          while(weight < 0)
+          {
+            rate += 0.15;
+            weight--;
+          }
+      break;
+      case 'LargeEnvelopes':
+        if(weight < 1)
+          {
+              rate = 1;
+              weight--;
+          }
+          while(weight < 0)
+          {
+            rate += 0.15;
+            weight--;
+          }
+      break;
+      case 'FirstClass':
+          if(weight < 5) {
+            rate = 3.66;
+          } else if (weight < 9) {
+            rate = 4.39;
+          } else if (weight < 13) {
+            rate = 5.19;
+          } else {
+            rate = 5.71;
+          }
+
+      break;
+    }
+    return { mailType: mailType, weight: weight, rate: rate };
+
+  }
 }
